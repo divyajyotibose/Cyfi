0820e35df88e6d608bc926c960ea2e08395e1d0e
dj <divyajyotibose@gmail.com>
2023-03-14 20:51:44 +0530

    Update Palette.dart

--- cyfi/lib/config/Palette.dart
+++ cyfi/lib/config/Palette.dart
@@ -11,19 +11,6 @@ class Palette {
   static Color dark = const Color(0xFF000000);
   static Color light = const Color(0xFFFFFFFF);
 
-  static Color bgColor = const Color(0xFF03001C);
-  static Color barColor = const Color(0xFF03001C);
-  static Color barTextColor = const Color(0xFFffffff);
-  static Color bgTextColor = const Color(0xFF5B8FB9);
-  static Color DrawerHeaderColor = const Color(0xFFF0DFCB);
-  static Color searchBarColor = const Color(0xFFFFFFFF);
-  static Color ListColor = const Color(0xFF301E67);
-  static Color checkColor = const Color(0xFFFFFFFF);
-  static Color fillColor = const Color(0xFF000000);
-  static Color buttonColor = const Color(0xFF000000);
-  static Color cardColor = const Color(0xFFFFFFFF);
-  static Color selColor = const Color(0xFFFFFFFF);
-
   static EdgeInsets mainPadding = const EdgeInsets.all(10);
 
   static TextStyle mainTitle = TextStyle(
